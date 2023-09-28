d62c98455d716117a2ddf2e2d5db67879065a8d9
Samuel Sukovský <145551742+SamuelSukovsky@users.noreply.github.com>
2023-09-28 10:27:02 +0100

    ~GRAVITY~

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello, world!");
+        
     }
 
     // Update is called once per frame
